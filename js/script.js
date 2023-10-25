b93301ff4188600401b4d3ffe9e2dd503a7352b5
marcoossmb_ <marcossblazquezz@gmail.com>
2023-10-26 00:14:46 +0200

    tokens, dice and launch button created

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 let bienvenida = document.getElementById("bienvenida")
 let tablero = document.getElementById("tablero")
 let bienvenida__button = document.getElementById("bienvenida__button")
+let juego = document.getElementById("juego")
+let dado1 = document.getElementById("dado1")
+let dado2 = document.getElementById("dado2")
+let dado__button = document.getElementById("dado__button") 
 
 bienvenida__button.addEventListener("click",()=>{
     bienvenida.classList.add("ocultar")
     tablero.classList.add("mostrar")
+    juego.classList.add("mostrardados")
 })
