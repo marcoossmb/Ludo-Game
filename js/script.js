96ab686933e3ac0f9be6e200751b874f76645cfe
marcoossmb_ <marcossblazquezz@gmail.com>
2023-10-25 14:01:44 +0200

    hide and show welcome menu and dashboard with button

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
+let bienvenida = document.getElementById("bienvenida")
+let tablero = document.getElementById("tablero")
+let bienvenida__button = document.getElementById("bienvenida__button")
 
+bienvenida__button.addEventListener("click",()=>{
+    bienvenida.classList.add("ocultar")
+    tablero.classList.add("mostrar")
+})
